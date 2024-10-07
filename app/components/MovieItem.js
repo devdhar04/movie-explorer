ec4f1af7ffcc6e1254fb09c6fcb0bb70b07348c6
Dev <>
2024-10-07 15:31:36 +0200

    Update MovieItem.js

--- app/components/MovieItem.js
+++ app/components/MovieItem.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,memo } from 'react';
 import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 import { Link } from 'expo-router';
 import FavoriteButton from './FavoriteButton';
@@ -10,7 +10,7 @@ const MovieItem = ({ movie, genres }) => {
 
   useEffect(() => {
     getGenreNamesById(movie.genre_ids, genres).then((storedGenres) => {
-    setGenreList(storedGenres);  
+      setGenreList(storedGenres);
     });
   }, []);
 
@@ -33,19 +33,25 @@ const MovieItem = ({ movie, genres }) => {
             source={{ uri: `https://image.tmdb.org/t/p/w200${movie.poster_path}` }}
           />
           <View style={styles.info}>
-            <Text style={styles.title}>{movie.title}</Text>
+            <Text style={styles.title} numberOfLines={2} ellipsizeMode='tail'>{movie.title}</Text>
             <Text style={styles.releaseDate}>{movie.release_date}</Text>
             <View style={styles.favouriteContainer}>
               <Text style={styles.releaseDate}>{movie.vote_average}</Text>
-              <FavoriteButton style={styles.fav} movie = {movie}/>
+
             </View>
-            
+
             <Text style={styles.genre}>
               {genreList}
             </Text>
           </View>
         </View>
+
       </Link>
+
+      <View style={{ position: 'absolute', top: 60, right: 20, }} >
+        <FavoriteButton  movie={movie} />
+      </View>
+
     </TouchableOpacity>
   );
 };
@@ -54,23 +60,23 @@ const styles = StyleSheet.create({
   link: { marginBottom: 10 },
   movieItem: { flexDirection: 'row', marginBottom: 10 },
   poster: { width: 100, height: 150, borderRadius: 10 },
-  info: { marginLeft: 10, justifyContent: 'center' },
-  title: { fontSize: 18, fontWeight: 'bold' },
+  info: { marginLeft: 10, justifyContent: 'center',},
+  title: { fontSize: 18, fontWeight: 'bold',width: '65%', paddingRight:10 },
   releaseDate: { color: 'gray' },
   favouriteContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    
+
   },
   genre: {
-    width:'100%',
+    width: '65%',
     flexDirection: 'row',
+    marginRight:10
   },
   fav: {
-    left: 0,
-    right: 0,
-    position: 'absolute',
+    position: 'absolute', top: 20, right: 20,
+    width: 50, height: 50
   }
 });
 
-export default MovieItem;
+export default memo(MovieItem);
